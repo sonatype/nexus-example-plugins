249b16f1044698d79b373c6a43328f1598549d25
Peter Lynch <peter@peterlynch.ca>
2014-08-25 16:14:55 -0300

    update header year

--- selectionactors/src/main/java/org/sonatype/nexus/examples/selectionactors/inmemory/SimpleSelectionEntry.java
+++ selectionactors/src/main/java/org/sonatype/nexus/examples/selectionactors/inmemory/SimpleSelectionEntry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007-2012 Sonatype, Inc. All rights reserved.
+ * Copyright (c) 2007-2014 Sonatype, Inc. All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0,
  * and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -10,7 +10,6 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-
 package org.sonatype.nexus.examples.selectionactors.inmemory;
 
 import org.sonatype.nexus.examples.selectionactors.SelectionEntry;
